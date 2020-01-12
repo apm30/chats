78697a9c218e0d1aa0a753357d73556283822211
Domo <stolarczyk.dominik@gmail.com>
2020-01-12 12:48:04 +0100

    final

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -42,7 +42,7 @@ export default function Dashboard() {
 	const classes = useStyles();
 
 	//CTX Store
-	const [allChats] = React.useContext(CTX);
+	const { allChats, sendChatAction, user } = React.useContext(CTX);
 	const topics = Object.keys(allChats);
 
 	//local state
@@ -95,6 +95,14 @@ export default function Dashboard() {
 						variant="contained"
 						color="primary"
 						className={classes.button}
+						onClick={() => {
+							sendChatAction({
+								from: user,
+								msg: textValue,
+								topic: activeTopic
+							});
+							changeTextValue('');
+						}}
 					>
 						Send
 					</Button>
